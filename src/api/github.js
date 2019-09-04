7c8fa44c86f108fd8c3f4f76961dbdda6d7feec9
Salvatore Ventrone <ventrosky@gmail.com>
2019-09-04 19:25:51 +0200

    Repos Languages
    Added language lists for all of the owner's repositories

--- src/api/github.js
+++ src/api/github.js
@@ -3,36 +3,40 @@
 import octokit from './octo';
 
 const USERNAME = "Ventrosky";
-/*
-exports.getRepoLangs = async (languages_url)=>{
-  return axios.get(languages_url)
-   .then(response => {
-      console.log(resp.data);
-      return response.data;
-   })
-   .catch(error => {
-     console.log(error);
-   });
-}*/
 
 exports.octoUserRepos = (name)=>{
-  return octokit.repos.listForUser({
-    "per_page": 100,
-    "username": name || USERNAME
-  })
-  .then(resp=>{
-    console.log(resp.data);
-    return resp.data.map(r=>{
-      return {
-      "id": r.id,
-      "full_name": r.full_name,
-      "description":r.description,
-      "name": r.name,
-      "languages_url": r.languages_url,
-      "html_url":r.html_url,
-      "fork": r.fork
-      }})})
-  .catch(ex=> console.log(ex));
+    const getRepoLangs = async (obj, repo, name)=>{
+        return await octokit.repos.listLanguages({
+            "owner": name || USERNAME,
+            repo
+        }).then(resp => {
+          console.log(obj);
+          return Object.assign(obj, {"languages": resp.data});
+       })
+       .catch(error => {
+         console.log(error);
+       });
+    }
+    let user = name || USERNAME;
+    return octokit.repos.listForUser({
+        "per_page": 100,
+        "username": user
+    })
+    .then(async resp=>{
+        console.log(resp.data);
+        return Promise.all(resp.data.map(async r=>{
+            return Promise.resolve(await getRepoLangs({
+                "id": r.id,
+                "full_name": r.full_name,
+                "description":r.description,
+                "name": r.name,
+                "languages_url": r.languages_url,
+                "html_url":r.html_url,
+                "fork": r.fork
+                }, r.name, user))
+            }))
+        })
+    .catch(ex=> console.log(ex));
 }
 
 exports.octoUserEvents = (name)=>{
